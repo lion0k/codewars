ece3cf118a6b1ddb315435c71d9e8b8147f5c40d
lion0k <lion_ok@mail.ru>
2020-09-01 22:03:33 +0300

    Update roman_numerals_helper.py

--- roman_numerals_helper.py
+++ roman_numerals_helper.py
@@ -1,21 +1,42 @@
 class RomanNumerals:
-    info = {'I': 1, 'II': 2, 'III': 3, 'IV': 4, 'V': 5, 'VI': 6, 'VII': 7, 'VIII': 8, 'IX': 9, 'X': 10,
+    roman = {'I': 1, 'II': 2, 'III': 3, 'IV': 4, 'V': 5, 'VI': 6, 'VII': 7, 'VIII': 8, 'IX': 9, 'X': 10,
             'XL': 40, 'L': 50, 'XC': 90, 'C': 100, 'CD': 400, 'D': 500, 'CM': 900, 'M': 1000}
 
-    def _get_roman_from_dic(self, x):
-        if x == '0':
-            return ''
-        for key, val in self.info.items():
-            if val == int(x):
-                return key
-        return ''
+    arab = {v: k for k, v in roman.items()}
+
+    def _get_arab_from_dic(self, x):
+        print(x)
+        near = 0
+        int_x = int(x)
+        for key, val in self.arab.items():
+            if near <= key:
+                near = key
+            if key == int_x:
+                return val
+        if len(x) == 2:
+            if 10 <= int_x < 40:
+                return self.arab[10] * int(x[:1])
+            else:
+                return self.arab[50] + self.arab[10] * (int(x[:1]) - 5)
+        elif len(x) == 3:
+            if 100 <= int_x < 400:
+                return self.arab[100] * int(x[:1])
+            else:
+                return self.arab[500] + self.arab[100] * (int(x[:1]) - 5)
+        else:
+            return self.arab[1000] * int(x[3:])
 
     def to_roman(self, x):
         inp = str(x)[::-1]
         out = []
         for i in range(0, len(inp)):
-            out.append(inp[i] + '0' * i)
-        return out
+            curr = inp[i] + '0' * i
+            if i == 0 and inp[i] == '0':
+                continue
+            elif inp[i] == '0':
+                curr = '1' + curr
+            out.append(self._get_arab_from_dic(curr))
+        return ''.join(out[::-1])
 
 
 
@@ -24,7 +45,7 @@ class RomanNumerals:
         pass
 
 
-print(RomanNumerals().to_roman(1990))
+print(RomanNumerals().to_roman(2008))
 
 #
 #
